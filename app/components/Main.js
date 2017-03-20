4c5bcd9e5fdf5ed0e7dfa1306af66448f396f8a8
alejandro harb <alejandro harb>
2017-03-20 11:59:32 -0500

    started Main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,7 +2,22 @@ var React = require('react');
 
 class Main extends React.Component {
     render() {
-        return();
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Movie Info</h3>
+                            </div>
+                            <div className="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
