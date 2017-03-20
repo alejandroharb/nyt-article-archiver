b2b808a9789d562ab0328a545b4dd33f80dfdae4
Alejandro Harb <alejandroharb@gmail.com>
2017-03-20 12:32:52 -0500

    fixed webpack error

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,22 +1,10 @@
 var React = require('react');
+import Search from './Search';
 
 class Main extends React.Component {
     render() {
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">
-                                <h3 className="panel-title">Movie Info</h3>
-                            </div>
-                            <div className="panel-body">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Search />
         );
     }
 }
---
+++ app/components/Search.js
@@ -0,0 +1,24 @@
+var React = require('react');
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Movie Info</h3>
+                            </div>
+                            <div className="panel-body">
+                                <p>Example of newest code</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search;
